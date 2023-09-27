496dd7428a4ec24ec60487e530476c512266b449
Brad Smith <bradsmith@duck.com>
2023-09-27 16:23:04 -0400

    Add #1 (two versions) and some an <h1>

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@
       later on
 */
 
-function sayHello(yourName="world") {
+
+function sayHello(yourName = "world") {
   // code goes here
   console.log(`Hello, ${yourName}!`);
 }
@@ -18,6 +19,30 @@ function sayGoodbye(phrase, name) {
 }
 // sayGoodbye("See ya", 'Dwayne "The Rock" Johnson');
 
+
 function add(num1, num2) {
   console.log(num1 + num2);
 }
+
+
+/* =======================
+/* 1.
+/* =====================*/
+// function maxOfTwoNumbers(num1, num2) {
+//   if (num1 >= num2) {
+//     return num1;
+//   } else { 
+//     return num2;
+//   }
+// }
+
+// alt. version w/ ternary
+function maxOfTwoNumbers(num1, num2) {
+  return (num1 >= num2) ? num1 : num2;
+}
+console.log(maxOfTwoNumbers(135, 15));
+
+
+/* =======================
+/* 2.
+/* =====================*/
